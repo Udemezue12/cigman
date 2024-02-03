c9d16b423d22dc21ea7d02172e337fa7ab5d2eda
Uchechukwu Udemezue <udemezue008@gmail.com>
2024-02-03 02:17:41 +0100

    Modified some mistakes

--- portfolio_project/settings/common.py
+++ portfolio_project/settings/common.py
@@ -190,6 +190,7 @@ DJOSER = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
     'http://127.0.0.1:8001',
+    'https://cigman-store-ed64e91baede.herokuapp.com',
 ]
 
 CELERY_BROKER_URL = 'redis://localhost:6376/1'
